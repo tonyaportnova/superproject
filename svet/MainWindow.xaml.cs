74355a293c6e48c827099da0f24a2b2d63e1fd09
student <student@vyatsu.ru>
2018-10-02 09:59:12 +0300

    Создано очень полезное приложение

---
+++ svet/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace svet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double sum, diff, led, incandescent;
+            if (double.TryParse(sumTextBox.Text, out sum) && double.TryParse(diffTextBox.Text, out diff))
+            {
+                led = (sum - diff) / 2;
+                incandescent = led + diff;
+                status.Text = String.Format("Стоимость LED: {0}; энергосберегающей лампы: {1}", led, incandescent);
+                return;
+            }
+
+            status.Text = "ты втираешь мне какую-то дичь";
+        }
+    }
+}
